aacd36ea89a818f427c68e0dab421e943f98c985
Rumman Bin Ashraf <rumman@therapservices.net>
2024-03-29 21:38:56 +0600

    Added File Upload Functionality

--- complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,7 +1,12 @@
 package com.example.servingwebcontent;
 
+import com.example.service.StorageProperties;
+import com.example.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -10,10 +15,19 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan("com.example.entity")
 @ComponentScan("com.example")
 @EnableJpaRepositories("com.example.repository")
+@EnableConfigurationProperties(StorageProperties.class)
 public class ServingWebContentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 
 }
